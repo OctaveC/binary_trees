8d5d49f89c61de63505aeac6a0a258f5caa00dba
maba-dev <cherif.ba@hotmail.fr>
2021-07-19 16:16:54 +0200

    task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * 
+ * 
+ * 
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (!node)
+        return 0;
+    if (node->left || node->right)
+        return 0;
+    else
+        return 1;
+}

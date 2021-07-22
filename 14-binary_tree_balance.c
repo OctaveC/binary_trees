57205296984c3cd4bd849d987f13a57f12b3853a
octave collombel <octavec@outlook.com>
2021-07-22 14:02:30 +0200

    exo 14 weird issue

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,18 +1,38 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
 /**
- * 
- * 
- * 
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: pointer to the root node of the tree we want the balance of
  *
+ * Return: If tree is NULL, nothing
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (!tree)
-        return 0;
-    if (tree)
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	if (!tree)
+		return (0);
 
-	return (0);
-	
+	return (binary_tree_height_3(tree->left) - binary_tree_height_3(tree->right));
+}
+
+/**
+ * binary_tree_height_3 - Measures the height of a binary tree.
+ * @tree: pointer to the root node of the tree we want the height of
+ *
+ * Return: If tree is NULL, nothing
+ */
+
+size_t binary_tree_height_3(const binary_tree_t *tree)
+{
+	int height_l, height_r;
+
+	if (tree == NULL)
+		return (0);
+
+	height_l = binary_tree_height_3(tree->left);
+	height_r = binary_tree_height_3(tree->right);
+
+	if (height_l >= height_r)
+		return (height_l + 1);
+	else
+		return (height_r + 1);
 }
